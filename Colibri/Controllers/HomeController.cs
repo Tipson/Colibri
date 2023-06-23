ede5c8925b4be308fba224cae96046ac7f3bb57e
Tipso <tipson@live.ru>
2023-06-23 18:02:09 +0300

    Update HomeController.cs

--- Colibri/Controllers/HomeController.cs
+++ Colibri/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
 ﻿using System.Diagnostics;
+using System.Globalization;
 using Colibri.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Colibri.Models;
+using Colibri.Models.Commands.Portfolio;
+using Colibri.Models.Commands.Product;
+using Colibri.Models.Commands.Review;
+using Colibri.Models.Commands.Statistic;
 using Colibri.Models.Commands.TeamMember;
 using Colibri.Models.TeamMembers;
 
@@ -11,15 +16,29 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly ITeamMemberService _teamMemberService;
+    private readonly IStatisticService _statisticService;
+    private readonly IReviewService _reviewService;
+    private readonly IProductService _productService;
+    private readonly IPortfolioService _portfolioService;
 
-    public HomeController(ILogger<HomeController> logger, ITeamMemberService teamMemberService)
+    public HomeController(ILogger<HomeController> logger, ITeamMemberService teamMemberService, IPortfolioService portfolioService, IProductService productService, IReviewService reviewService, IStatisticService statisticService)
     {
         _logger = logger;
         _teamMemberService = teamMemberService;
+        _portfolioService = portfolioService;
+        _productService = productService;
+        _reviewService = reviewService;
+        _statisticService = statisticService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index(CancellationToken token = default)
     {
+        ViewBag.TeamMembers = await _teamMemberService.GetAll(new GetAllTeamMemberCommand(), token);
+        ViewBag.Statistics = await _statisticService.GetAll(new GetAllStatisticCommand(), token);
+        ViewBag.Porfolios = await _portfolioService.GetAll(new GetAllPortfolioCommand(), token);
+        ViewBag.Favorites = await _reviewService.GetAll(new GetAllReviewCommand(), token);
+        ViewBag.Topics = await _productService.GetAll(new GetAllProductCommand(), token);    
+        
         return View();
     }
 
