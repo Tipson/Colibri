a58ab3c522821e91a3555a962f6a68c009a12107
Tipso <tipson@live.ru>
2023-06-29 01:51:33 +0300

    Доработано представление для Manage

--- Colibri/Utils/Select.cs
+++ Colibri/Utils/Select.cs
@@ -10,9 +10,9 @@ public class Select
     {
          Dictionary<string, dynamic> tableElem = new Dictionary<string, dynamic>();
          TableColumn[] columns = new TableColumn[] {
-             new TableColumn { caption= viewLocalizer["Name"].Value, id = "name", sort_by = "s_name", procent=25},
-             new TableColumn { caption= viewLocalizer["Position"].Value, id = "position", sort_by = "s_position", procent=25},
-             new TableColumn { caption= viewLocalizer["Type"].Value, id = "type", sort_by = "s_type", procent=25},
+             new TableColumn { caption= viewLocalizer["Id"].Value, id = "name", sort_by = "s_name", procent=25},
+             new TableColumn { caption= viewLocalizer["BrandName"].Value, id = "position", sort_by = "s_position", procent=25},
+             new TableColumn { caption= viewLocalizer["Description"].Value, id = "type", sort_by = "s_type", procent=25},
              new TableColumn { caption= "", id = "", sort_by = "", procent=25},
          };
          tableElem["Columns"] = columns;
@@ -26,8 +26,8 @@ public class Select
      {
          Dictionary<string, dynamic> tableElem = new Dictionary<string, dynamic>();
          TableColumn[] columns = new TableColumn[] {
-             new TableColumn { caption= viewLocalizer["Name"].Value, id = "name", sort_by = "p_name", procent=50},
-             new TableColumn { caption= viewLocalizer["Type"].Value, id = "type", sort_by = "p_type", procent=25},
+             new TableColumn { caption= viewLocalizer["Title"].Value, id = "name", sort_by = "p_name", procent=50},
+             new TableColumn { caption= viewLocalizer["Price"].Value, id = "type", sort_by = "p_type", procent=25},
              new TableColumn { caption= "", id = "", sort_by = "", procent=25},
          };
          tableElem["Columns"] = columns;
@@ -53,33 +53,13 @@ public class Select
          return tableElem;
      }
 
-     public static Dictionary<string, dynamic> GetAgendasTable(IViewLocalizer viewLocalizer, string listPath = "_agendaList")
+     public static Dictionary<string, dynamic> GetStatisticsTable(IViewLocalizer viewLocalizer, string listPath = "_statisticList")
      {
          Dictionary<string, dynamic> tableElem = new Dictionary<string, dynamic>();
          TableColumn[] columns = new TableColumn[] {
-             new TableColumn { caption= viewLocalizer["SpeakersName"].Value, id = "name", sort_by = "p_name", procent=60},
-             new TableColumn { caption= viewLocalizer["Topics"].Value, id = "type", sort_by = "p_type", procent=20},
-             new TableColumn { caption= viewLocalizer["TimeStart"].Value, id = "type", sort_by = "p_type", procent=10},
-             new TableColumn { caption= viewLocalizer["TimeEnd"].Value, id = "type", sort_by = "p_type", procent=10},
-
-         };
-         tableElem["Columns"] = columns;
-         tableElem["List"] = listPath;
-         tableElem["PageSize"] = 3000;
-         tableElem["TableClass"] = "";
-         return tableElem;
-     }
-     
-     public static Dictionary<string, dynamic> GetSponsorPacksTable(IViewLocalizer viewLocalizer, string listPath = "_sponsorPackList")
-     {
-         Dictionary<string, dynamic> tableElem = new Dictionary<string, dynamic>();
-         TableColumn[] columns = new TableColumn[] {
-             new TableColumn { caption= viewLocalizer["Name"].Value, id = "name", sort_by = "p_name", procent=25},
-             new TableColumn { caption= viewLocalizer["Description"].Value, id = "type", sort_by = "p_type", procent=20},
-             new TableColumn { caption= viewLocalizer["Total Stand"].Value, id = "type", sort_by = "p_type", procent=15},
-             new TableColumn { caption= viewLocalizer["Reserved Stand"].Value, id = "type", sort_by = "p_type", procent=15},
-
-
+             new TableColumn { caption= viewLocalizer["Name"].Value, id = "name", sort_by = "s_name", procent=25},
+             new TableColumn { caption= viewLocalizer["Count Followers"].Value, id = "position", sort_by = "s_position", procent=25},
+             new TableColumn { caption= viewLocalizer["Count Views"].Value, id = "type", sort_by = "s_type", procent=25},
              new TableColumn { caption= "", id = "", sort_by = "", procent=25},
          };
          tableElem["Columns"] = columns;
@@ -89,16 +69,13 @@ public class Select
          return tableElem;
      }
      
-     public static Dictionary<string, dynamic> GetLocationsTable(IViewLocalizer viewLocalizer, string listPath = "_locationList")
+     public static Dictionary<string, dynamic> GetTeamMembersTable(IViewLocalizer viewLocalizer, string listPath = "_teamMemberList")
      {
          Dictionary<string, dynamic> tableElem = new Dictionary<string, dynamic>();
          TableColumn[] columns = new TableColumn[] {
-             new TableColumn { caption= viewLocalizer["Name"].Value, id = "name", sort_by = "p_name", procent=25},
-             new TableColumn { caption= viewLocalizer["X"].Value, id = "type", sort_by = "p_type", procent=25},
-             new TableColumn { caption= viewLocalizer["Y"].Value, id = "type", sort_by = "p_type", procent=25},
-             new TableColumn { caption= viewLocalizer["Partner"].Value, id = "type", sort_by = "p_type", procent=25},
-
-
+             new TableColumn { caption= viewLocalizer["Name"].Value, id = "name", sort_by = "s_name", procent=25},
+             new TableColumn { caption= viewLocalizer["Position"].Value, id = "position", sort_by = "s_position", procent=25},
+             new TableColumn { caption= viewLocalizer["Linkedin"].Value, id = "type", sort_by = "s_type", procent=25},
              new TableColumn { caption= "", id = "", sort_by = "", procent=25},
          };
          tableElem["Columns"] = columns;
@@ -107,4 +84,5 @@ public class Select
          tableElem["TableClass"] = "";
          return tableElem;
      }
+     
 }
